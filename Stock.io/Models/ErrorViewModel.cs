8c4c7ca4ac3c33a40d2c020287f05392b4395330
David Gonzalez <davidgld@icloud.com>
2019-01-23 22:28:17 -0500

    Add project files.

---
+++ Stock.io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stock.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
